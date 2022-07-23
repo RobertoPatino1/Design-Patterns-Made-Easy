1c1e5b34bf7912f2aab1866cba2be936e324806f
RobertoPatino1 <93289504+RobertoPatino1@users.noreply.github.com>
2022-07-23 11:53:23 -0500

    Uploaded Director class

---
+++ CreationalDesignPatterns/Builder/director/Kitchen.java
@@ -0,0 +1,35 @@
+package builder;
+
+
+public class Kitchen {
+    private Builder builder;
+
+    public Kitchen(Builder builder){
+        this.builder = builder;
+    }
+
+    //Building methdos aplied
+    public void buildHawaiianPizza(){
+        builder.setMace("Soft");
+        builder.setSalsa("Sweet pineaple salsa");
+        builder.setFilling("Pineaple slices, cheese");
+    }
+
+    public void buildPepperoniPizza(){
+        builder.setMace("Cooked");
+        builder.setSalsa("Traditional salsa");
+        builder.setFilling("Pepperoni slices, cheese");
+    }
+
+    public void buildMeatPizza(){
+        builder.setMace("Cooked");
+        builder.setSalsa("Spicy chilli salsa");
+        builder.setFilling("Meat, cheese, ham");
+    }
+
+
+    public Pizza getPizza(){
+        return builder.getResult();
+    }
+
+}
